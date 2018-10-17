f6e6c2609d96f609365900da33bd57940f701f56
= <vitorleoneloficial@gmail.com>
2018-10-17 13:35:20 -0300

    add cors and nodemon'

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ require('dotenv').config();
 
 const app = require('express')();
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const config = require('./config');
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.json({ message: 'API in operation.' });
