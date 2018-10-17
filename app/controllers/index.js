fbc08b64faa75af3eb3856a43554393c9bad6ed2
= <vitorleoneloficial@gmail.com>
2018-10-17 14:04:01 -0300

    init routes

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,1 +1,5 @@
-module.exports = {}
+const authController = require('./authController');
+
+module.exports = {
+  authController,
+}
--- index.js
+++ index.js
@@ -5,16 +5,15 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
+
 const config = require('./config');
+const routes = require('./app/routes');
 
 mongoose.connect(config.database.url, { useNewUrlParser: true });
 requireDir(config.database.modelsPath);
 
 app.use(bodyParser.json());
 app.use(cors());
-
-app.get('/', (req, res) => {
-  res.json({ message: 'API in operation.' });
-});
+app.use('/', routes);
 
 app.listen(config.app.port);
